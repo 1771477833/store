e2062cb2496d9edbdda708a410f62cf69d9c61cb
1771477833 <92710157+1771477833@users.noreply.github.com>
2021-10-20 16:42:47 +0800

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,34 @@
+'''
+猜字游戏：
+需求：
+1、猜的数字是系统产生的，不是自己定义的
+2、键盘输入的   操作完填入：input（“提示”）
+3、判断			操作完填入：if判断条件 elif 判断条件。。。。。。Else
+4、循环			操作完填入：while 条件循环
+#任务：开始金币有5个金币，每猜一次减一个为0就退出（or充钱）猜错3次也退出
+'''
+import random
+randint = random.randint(10, 20)  # 随机生成数字的范围：int   []
+print(randint)
+i=3
+n=5
+while i>=1:
+    print("你还有%d次机会"%i)
+    print("您还有%d个金币"%n)
+    num=int(input("请输入您猜的数字"))
+    if num==randint:
+        print("恭喜你猜对了")
+        n=n+3
+        randint = random.randint(10, 20)  # 随机生成数字的范围：int   []
+        print(randint)
+        continue
+    elif num>randint:
+        print("猜大了")
+    elif num<randint:
+        print("猜小了")
+    else:
+        print("猜错了,继续猜")
+    i=i-1
+    n=n-1
+print("您还剩余%d个金币"%n)
+
